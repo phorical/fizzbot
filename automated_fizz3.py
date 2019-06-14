15e9cfdeedc352609c6b9adefa1546a6e0d65196
Brady Hahn <hahn.brady@gmail.com>
2019-06-13 21:01:41 -0400

    cleaned up

--- automated_fizz3.py
+++ automated_fizz3.py
@@ -6,8 +6,6 @@ import urllib.error
 
 domain = 'https://api.noopschallenge.com'
 
-def print_sep(): print('----------------------------------------------------------------------')
-
 # print server response
 def print_response(dict):
     print('')
@@ -18,83 +16,6 @@ def print_response(dict):
         if key != 'message':
             print('%s: %s' % (key, json.dumps(dict.get(key))))
     print('')
-
-# try an answer and see what fizzbot thinks of it
-def try_answer(question_url, answer):
-    print_sep()
-    body = json.dumps({ 'answer': answer })
-    print('*** POST %s %s' % (question_url, body))
-    try:
-        req = urllib.request.Request(domain + question_url, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
-        res = urllib.request.urlopen(req)
-        response = json.load(res)
-        print_response(response)
-        print_sep()
-        return response
-
-    except urllib.error.HTTPError as e:
-        response = json.load(e)
-        print_response(response)
-        return response
-
-# keep trying answers until a correct one is given
-def get_correct_answer(question_url):
-    while True:
-        answer = input('Enter your answer:\n')
-
-        response = try_answer(question_url, answer)
-
-        if (response.get('result') == 'interview complete'):
-            print('congratulations!')
-            exit()
-
-        if (response.get('result') == 'correct'):
-            input('press enter to continue')
-            return response.get('nextQuestion')
-
-# do the next question
-def do_question(domain, question_url):
-
-    request = urllib.request.urlopen( ('%s%s' % (domain, question_url)) )
-    question_data = json.load(request)
-    print(question_data)
-    numbers = question_data.get('numbers')
-
-    if (numbers) :
-        string = ''
-        it = iter(numbers)
-        try :
-            currentNumber = next(it)
-            boolean = False
-            if (currentNumber % 3 == 0):
-                string += 'Fizz'
-                boolean = True
-            if (currentNumber % 5 == 0):
-                string += 'Buzz'
-                boolean = True
-            if not boolean:
-                string += currentNumber
-            string += ' '
-        except StopIteration:
-            string[0,len(string)-2]
-        response = try_answer(question_url, string)
-
-    req = urllib.request.Request(domain + question_url, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
-    res = urllib.request.urlopen(req)
-    """
-    print_sep()
-    print('*** GET %s' % question_url)
-
-    request = urllib.request.urlopen( ('%s%s' % (domain, question_url)) )
-    question_data = json.load(request)
-    print_response(question_data)
-    print_sep()
-
-    next_question = question_data.get('nextQuestion')
-
-    if next_question: return next_question
-    return get_correct_answer(question_url)
-    """
 
 def apply_rules(number, rules):
     ret = ''
@@ -116,12 +37,8 @@ def make_rules(rules):
 def do_next_question(domain, question_url):
     request = urllib.request.urlopen( ('%s%s' % (domain, question_url)) )
     question_data = json.load(request)
-    #print(question_data.get('message'))
-    #print(question_data.get('rules'))
     rules = make_rules(question_data.get('rules'))
     numbers = question_data.get('numbers')
-    #print(numbers)
-    #print(rules)
     body = None
     if (numbers) :
         string = ''
@@ -133,14 +50,10 @@ def do_next_question(domain, question_url):
                 string += ' '
         except StopIteration:
             string = string[0:-1]
-        #print(string)
         body = json.dumps({ 'answer':string })
-    print(body + " to " + domain + " " + question_url)
     req = urllib.request.Request(domain + question_url, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
-    #print(req.get_full_url())
     res = urllib.request.urlopen(req)
     response = json.load(res)
-    #print(response)
     nextQuestion = response.get('nextQuestion')
     if nextQuestion:
         return do_next_question(domain, nextQuestion)
@@ -153,12 +66,8 @@ def first_question(domain, question_url):
         firstRawResponse = urllib.request.urlopen( ('%s%s' % (domain, question_url)) )
         firstParsedResponse = json.load(firstRawResponse)
         firstQuestionUrl = firstParsedResponse.get('nextQuestion')
-        #postFirstAnswer = urllib.request.Request(domain + firstQuestionUrl, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
-        #firstQuestionResponse = urllib.request.urlopen(postFirstAnswer)
-        #parsedfirstQuestionResponse = json.load(firstQuestionResponse)
-        #secondQuestionUrl = parsedfirstQuestionResponse.get('nextQuestion')
-        postSecondAnswer = urllib.request.Request(domain + firstQuestionUrl, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
-        secondQuestionResponse = urllib.request.urlopen(postSecondAnswer)
+        firstPostAnswer = urllib.request.Request(domain + firstQuestionUrl, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
+        secondQuestionResponse = urllib.request.urlopen(firstPostAnswer)
         return json.load(secondQuestionResponse).get('nextQuestion')
     except urllib.error.HTTPError as e:
         response = json.load(e)
@@ -172,7 +81,5 @@ def main():
     #print(next_question)
     do_next_question(domain, next_question)
     return
-    while question_url:
-        question_url = do_question(domain, question_url)
 
 if __name__ == '__main__': main()
