351a8eb15c761ecf607f889b962191fc8ed51027
Brady Hahn <hahn.brady@gmail.com>
2019-06-13 20:16:31 -0400

    still doesn't work

--- fizz3.py
+++ fizz3.py
@@ -94,23 +94,57 @@ def do_question(domain, question_url):
     if next_question: return next_question
     return get_correct_answer(question_url)
     """
+def do_next_question(domain, question_url):
+    request = urllib.request.urlopen( ('%s%s' % (domain, question_url)) )
+    question_data = json.load(request)
+    print(question_data.get('message'))
+    numbers = question_data.get('numbers')
+    body = None
+    if (numbers) :
+        string = ''
+        it = iter(numbers)
+        try :
+            currentNumber = next(it)
+            boolean = False
+            if (currentNumber % 3 == 0):
+                string += 'Fizz'
+                boolean = True
+            if (currentNumber % 5 == 0):
+                string += 'Buzz'
+                boolean = True
+            if not boolean:
+                string += currentNumber
+            string += ' '
+        except StopIteration:
+            string[0,len(string)-2]
+        body = json.dumps({'answer': string})
+    req = urllib.request.Request(domain + question_url, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
+    res = urllib.request.urlopen(req)
+    response = json.load(res)
+    nextQuestion = res.get('nextQuestion')
+    return do_next_question(domain, nextQuestion)
+
 
 def first_question(domain, question_url):
     body = json.dumps({'answer':'COBOL'})
     try:
-        request = urllib.request.Request(domain + question_url, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
-        res = urllib.request.urlopen(request)
-        response = json.load(res)
-        return response
+        firstRawResponse = urllib.request.urlopen( ('%s%s' % (domain, question_url)) )
+        firstParsedResponse = json.load(firstRawResponse)
+        firstQuestionUrl = firstParsedResponse.get('nextQuestion')
+        postFirstAnswer = urllib.request.Request(domain + firstQuestionUrl, data=body.encode('utf8'), headers={'Content-Type': 'application/json'})
+        firstQuestionResponse = urllib.request.urlopen(postFirstAnswer)
+        return json.load(firstQuestionResponse).get('nextQuestion')
     except urllib.error.HTTPError as e:
         response = json.load(e)
         print_response(response)
-        return response
+        exit()
     return
 
 def main():
     question_url = '/fizzbot'
-    first_question(domain, question_url)
+    next_question = first_question(domain, question_url)
+    print(next_question)
+    do_next_question(domain, next_question)
     return
     while question_url:
         question_url = do_question(domain, question_url)
